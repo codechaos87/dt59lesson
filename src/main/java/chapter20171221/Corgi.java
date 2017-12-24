8846cef2381abae86836fc3d771337d8fff28858
chaos <chaos@chaos>
2017-12-24 22:34:23 +0800

    homework

---
+++ src/main/java/chapter20171221/Corgi.java
@@ -0,0 +1,43 @@
+/**
+ * Project Name:dt59lesson
+ * File Name:Corgi.java
+ * Package Name:chapter20171221
+ * Date:2017年12月24日下午12:49:50
+ * Copyright (c) 2017, bluemobi All Rights Reserved.
+ */
+
+package chapter20171221;
+
+/**
+ * Description: <br/>
+ * Date: 2017年12月24日 下午12:49:50 <br/>
+ * 
+ * @author chaos
+ * @version
+ * @see
+ */
+public class Corgi {// 柯基 一种狗
+    // 属性1 年龄
+    int age;
+
+    // 属性2 颜色
+    String color;
+
+    // 属性3 几条腿
+    int legs;
+
+    // 方法1
+    public void voice() {
+        System.out.println("汪汪汪");
+    }
+
+    // 方法2
+    public void sport() {
+        System.out.println("跑");
+    }
+
+    // 方法3
+    public void sleep() {
+        System.out.println("睡觉");
+    }
+}
---
+++ src/main/java/chapter20171221/Test.java
@@ -0,0 +1,30 @@
+/**
+ * Project Name:dt59lesson
+ * File Name:Test.java
+ * Package Name:chapter20171221
+ * Date:2017年12月24日下午1:01:42
+ * Copyright (c) 2017, bluemobi All Rights Reserved.
+ */
+
+package chapter20171221;
+
+/**
+ * Description: <br/>
+ * Date: 2017年12月24日 下午1:01:42 <br/>
+ * 
+ * @author chaos
+ * @version
+ * @see
+ */
+public class Test {
+    //
+    public static void main(String[] args) {
+
+        System.out.println(method(2));
+    }
+
+    public static int method(int num) {
+        return num * 3;
+    }
+
+}
