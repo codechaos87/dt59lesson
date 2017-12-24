cc0aed39c12762dc8e4f30448bf2b6ef37b4e650
chaos <chaos@chaos>
2017-12-24 22:45:54 +0800

    homeworkb

---
+++ src/main/java/chapter20171221/HomeWorkB.java
@@ -0,0 +1,29 @@
+/**
+ * Project Name:dt59lesson
+ * File Name:HomeWorkB.java
+ * Package Name:chapter20171221
+ * Date:2017年12月24日下午10:42:14
+ * Copyright (c) 2017, bluemobi All Rights Reserved.
+ */
+
+package chapter20171221;
+
+/**
+ * Description: <br/>
+ * Date: 2017年12月24日 下午10:42:14 <br/>
+ * 
+ * @author chaos
+ * @version
+ * @see
+ */
+public class HomeWorkB {
+
+    public static void main(String[] args) {
+
+        System.out.println(method(2));
+    }
+
+    public static int method(int num) {
+        return num * 3;
+    }
+}
