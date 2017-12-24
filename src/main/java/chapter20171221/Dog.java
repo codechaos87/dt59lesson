cf0e4bc6fe6f8dcfe8b10cd4ccad8ee0e99a0d53
chaos <chaos@chaos>
2017-12-24 22:44:24 +0800

    homework

---
+++ src/main/java/chapter20171221/Dog.java
@@ -0,0 +1,44 @@
+/**
+ * Project Name:dt59lesson
+ * File Name:Dog.java
+ * Package Name:chapter20171221
+ * Date:2017年12月24日下午10:41:22
+ * Copyright (c) 2017, bluemobi All Rights Reserved.
+ */
+
+package chapter20171221;
+
+/**
+ * Description: <br/>
+ * Date: 2017年12月24日 下午10:41:22 <br/>
+ * 
+ * @author chaos
+ * @version
+ * @see
+ */
+public class Dog {
+    // 属性1 年龄
+    int age;
+
+    // 属性2 颜色
+    String color;
+
+    // 属性3 几条腿
+    int legs;
+
+    // 方法1
+    public void voice() {
+        System.out.println("汪汪汪");
+    }
+
+    // 方法2
+    public void sport() {
+        System.out.println("跑");
+    }
+
+    // 方法3
+    public void sleep() {
+        System.out.println("睡觉");
+    }
+
+}
